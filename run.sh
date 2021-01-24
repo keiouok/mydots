ba050573431d3b089dffb4441977d89c64229935
keiouok <ko.yuka.kp2@is.naist.jp>
2021-01-24 16:53:04 +0900

    [RENAME] setup.sh -> run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+# link
 DOT_FILES=(.bashrc .bash_profile .vimrc .cshrc .emacs .gitconfig .tmux.conf .vim .zshrc) 
 
 for file in ${DOT_FILES[@]}
 do
     ln -s $HOME/mydots/$file $HOME/$file
 done
+
