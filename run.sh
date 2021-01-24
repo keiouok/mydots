61b5e06f889f0d9fbde74df10f2f0c00f57482f9
keiouok <ko.yuka.kp2@is.naist.jp>
2021-01-24 17:00:52 +0900

    [ADD] dein installer scripts in run.sh

--- run.sh
+++ run.sh
@@ -8,3 +8,8 @@ do
     ln -s $HOME/mydots/$file $HOME/$file
 done
 
+# dein (no link)
+# [ref] https://qiita.com/Coolucky/items/0a96910f13586d635dc0
+mkdir -p ~/.cache/dein
+curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > dein-installer.sh
+bash dein-installer.sh ~/.cache/dein
